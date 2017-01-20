a72580aa2cb5bbb182e34ebeb13b814a829296a1
Laura Folco <me@laurafolco.com>
2017-01-20 11:33:01 -0500

    add more to blacklist; attempt to toggle on/off via button

--- background.js
+++ background.js
@@ -1,20 +1,65 @@
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+/**
+ * Responds to clicks on the extension's icon. Toggles debug mode.
+ */
+
+// var debug = false;
+//
+// chrome.browserAction.onClicked.addListener(
+//     function (tab) {
+//         debug = !debug;
+//         chrome.browserAction.setTitle({
+//             title: debug ? 'Remove Trump: ON' : 'Remove Trump: OFF'
+//         });
+//         chrome.browserAction.setIcon({
+//             path: debug ? 'icon128.png' : 'icon128-off.png'
+//         });
+//         chrome.browserAction.setBadgeText({
+//             text: debug ? 'ON' : ''
+//         });
+//         chrome.tabs.update(tab.id, {url: tab.url, selected: tab.selected}, null);
+//         hasAPIs && chrome.webRequest.handlerBehaviorChanged();
+//     }
+// );
 
 
-var _gaq = _gaq || [];
+// function disableExtension(disabled)
+// {
+//     chrome.windows.getAll({populate : true}, function (window_list)
+//     {
+//         for (var i = 0; i < window_list.length; ++i)
+//         {
+//             var window = window_list[i];
+//             for (var j = 0; j < window.tabs.length; ++j)
+//             {
+//                 var tab = window.tabs[j];
+//                 if (checkContentScriptExists(tab))
+//                 {
+//                     chrome.tabs.executeScript(tab.id, {code : "disabled = " + disabled + ";"}, allTabs: true)
+//                 }
+//             }
+//         }
+//         // No matching url found. Open it in the new tab
+//         chrome.tabs.create({ url : url, selected: true });
+//     });
+// }
 
-_gaq.push(['_setAccount', 'UA-xxxxxxxxx-x']);
-_gaq.push(['_trackPageview']);
 
-chrome.runtime.onMessage.addListener(function( request, sender, sendResponse ) {
-    if(request.action == "removeTrump"){
-        _gaq.push(['_trackEvent', 'removeTrump', 'removed']);
-    }
-});
+// (function() {
+//     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+//     ga.src = 'https://ssl.google-analytics.com/ga.js';
+//     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+// })();
+
+// var _gaq = _gaq || [];
+//
+// _gaq.push(['_setAccount', 'UA-xxxxxxxxx-x']);
+// _gaq.push(['_trackPageview']);
+//
+// chrome.runtime.onMessage.addListener(function( request, sender, sendResponse ) {
+//     if(request.action == "removeTrump"){
+//         _gaq.push(['_trackEvent', 'removeTrump', 'removed']);
+//     }
+// });
 
 
 
--- facebook_filter.js
+++ facebook_filter.js
@@ -1,7 +1,3 @@
-function show(node, left) {
-    left = nodes[ii];
-}
-
 
 // init blacklist
 // var blacklist = [];// global array
@@ -32,14 +28,13 @@ function show(node, left) {
 //   });
 
 (function () {
-    var key = [
+    var blacklist = [
         'trump',
         'breitbart',
         'Steve_Bannon',
         'Mike Pence',
         'Mitch McConnel',
         'Paul Ryan',
-        'republicans',
         'Sean Spicer',
         'Rex Tillerson',
         'Steven Mnuchin',
@@ -55,22 +50,32 @@ function show(node, left) {
         'Betsy DeVos',
         'Ryan Zinke',
         'Wilbur Ross',
-        'David Shulkin'
+        'David Shulkin',
+        'Kellyanne Conway',
+        'Reince Priebus',
+        'republicans',
+        'congress',
+        'the senate',
+        'freedom caucus'
     ];
 
     var observer = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             var newNodes = mutation.addedNodes;
             if (newNodes !== null) {
+                // define nodes to check; these are facebook-specific class names
                 var nodes = document.querySelectorAll('.userContentWrapper, .userContent, ._5pcr, ._5pbx, ._1bar, ._5my2, ._4qjp, ._2kg4, ._5pco, ._52c6, ._5pbx, ._3x-2, .mtm');
+                // loop through nodes
                 for (var ii = 0, nn = nodes.length; ii < nn; ii++) {
+                    // get node content in lower case
                     var text = nodes[ii] ? nodes[ii].textContent.toLowerCase() : '';
                     if (text !== undefined) {
-                        for (var i = 0; i < key.length; i++) {
-                            if (text.toLowerCase().indexOf(key[i]) > 0 && nodes[ii].style.display != 'none') {
+                        // loop through the blacklist
+                        for (var i = 0; i < blacklist.length; i++) {
+                            // compare the text of the node to the blacklist entry
+                            if (text.toLowerCase().indexOf(blacklist[i].toLowerCase()) > 0 && nodes[ii].style.display != 'none') {
                                 nodes[ii].style.display = 'none';
                                 break;
-                                // chrome.runtime.sendMessage({action: "politicaRemovida"});
                             }
                         }
                     }
